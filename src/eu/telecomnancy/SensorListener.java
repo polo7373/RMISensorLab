8c5e5934da047c7c406e177ba5b59e2099a62457
Francois Charoy <charoy@gmail.com>
2013-09-27 08:32:22 +0200

    listener

---
+++ src/eu/telecomnancy/SensorListener.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.telecomnancy;
+
+/**
+ *
+ * @author charoy
+ */
+public interface SensorListener {
+    public void statusChanged();
+}
