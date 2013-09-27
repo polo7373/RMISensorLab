e5266faf8f0ac528ac05af31eefd8eced92da802
Francois Charoy <charoy@gmail.com>
2013-09-27 09:06:44 +0200

    client

--- src/eu/telecomnancy/Main.java
+++ src/eu/telecomnancy/Main.java
@@ -14,6 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ISensor s=new RandomSensor();
+        Client c=new Client(s);
+        c.menu();
     }
 }
